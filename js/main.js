e8817b0c0a3eed10786a1e6de033ff5c570515d7
Unknown <joseguerreroroman@yahoo.com.mx>
2018-03-23 11:40:59 -0600

    Cambios finales modales
    Se cambiaron los modales para incluir imágenes mejores

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $.getJSON("js/datos.json", function( json ) {
 });
 
 
-function crearOfertas(clave, valor) {
+function crearOfertas2(clave, valor) {
 	let valores = getInformacion[clave].val
 	let contenido = '<p class=\"lead text-center\"><strong>' + valores.nombre + '</strong></p><dl class=\"dl-horizontal lead\"><dt>Costo:</dt><dd>'
 	contenido += valores.costo +' en seís meses sin intereses</dd>'
@@ -39,6 +39,26 @@ function contenidoDatos(item, val){
     $('#writeDatos').html(contenido);
 
 };
+
+
+
+function crearOfertas(clave, valor) {
+	let valores = getInformacion[clave].val
+	let contenido = '<div class=\"row\"><div class=\"col-xs-10 col-xs-offset-1\">';
+	contenido += '<p class=\"lead text-center\"><strong>' + valores.nombre + '</strong></p>';
+	contenido += '<dl class=\"dl-horizontal lead\"><dt>Costo:</dt><dd>' + valores.costo + ' en seis meses sin intereses.</dd>';
+	contenido += '<dt>Duración: </dt><dd>' + valores.duracion + ' en los mejores hoteles</dd>';
+	contenido += '<dt>Oferta Especial:</dt><dd>' + valores.oferta + '</dd></dl></div></div>';
+	contenido += '<div class=\"row\"><div id=\"flexycontenedor\">';
+	contenido += '<div class=\"itemflex item-1\"><img src=\"' + valores.imagen1 + '\"></div>';
+	contenido += '<div class=\"itemflex item-1\"><img src=\"' + valores.imagen2 + '\"></div>';
+	contenido += '<div class=\"itemflex item-1\"><img src=\"' + valores.imagen3 + '\"></div>';
+	contenido += '</div></div>';
+	
+    $('#mainOfertas').html(contenido);
+}
+
+
 
 
 
